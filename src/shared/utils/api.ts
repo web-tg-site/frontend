b363765618e929eaadee8b89629c7b6289034df7
BainTheCreator <kottom615@gmail.com>
2025-11-30 16:45:53 +0700

    connect db

--- src/shared/utils/api.ts
+++ src/shared/utils/api.ts
@@ -7,6 +7,6 @@ const { API_HOST } = process.env;
 
 if(!API_HOST) throw new Error('Заполните хост апи');
 
-export const api = axios.create({
+export const $api = axios.create({
     baseURL: API_HOST
 })
