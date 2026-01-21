7491ff1fe38de6b77eb8b89f75cfef8a38d91af3
BainTheCreator <kottom615@gmail.com>
2026-01-21 23:01:39 +0700

    Убрал на время капчу

--- src/features/send-application-form/api/send-application.ts
+++ src/features/send-application-form/api/send-application.ts
@@ -4,5 +4,7 @@ import { SendApplicationFormData } from "../types/send-application-form-data";
 export const SendApplication = async (formData: SendApplicationFormData) => {
     const { data } = await $api.post('/send-notify', formData);
 
+    console.log(data);
+
     return data;
 }
