1549b57e0758b190310f5895ded330d3ad1bf2d2
BainTheCreator <kottom615@gmail.com>
2026-01-22 15:57:01 +0700

    поправил 400

--- src/features/send-application-form/api/send-application.ts
+++ src/features/send-application-form/api/send-application.ts
@@ -2,7 +2,8 @@ import { $api } from "@/shared/utils";
 import { SendApplicationFormData } from "../types/send-application-form-data";
 
 export const SendApplication = async (formData: SendApplicationFormData) => {
-    const { data } = await $api.post('/send-notify', formData);
+    const { isAgreed, ...payload } = formData;
+    const { data } = await $api.post('/send-notify', payload);
 
     return data;
 }
